1b295101ecc7a5ad01caefe1de2bc2e679862e50
Eyal Posener <posener@gmail.com>
2019-06-29 12:19:14 +0300

    Use contexttest

--- fcontext_test.go
+++ fcontext_test.go
@@ -1,81 +1,18 @@
 package fcontext
 
 import (
-	"sync"
 	"testing"
 
+	"github.com/posener/contexttest"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestValue_parentingGraph(t *testing.T) {
+func TestWithValue(t *testing.T) {
 	t.Parallel()
-
-	ctx0 := Background()
-	assert.Equal(t, nil, ctx0.Value(1))
-
-	ctx01 := WithValue(ctx0, 1, 1)
-	assert.Equal(t, 1, ctx01.Value(1))
-	assert.Equal(t, nil, ctx0.Value(1))
-
-	ctx02 := WithValue(ctx0, 2, 2)
-	assert.Equal(t, nil, ctx02.Value(1))
-	assert.Equal(t, 2, ctx02.Value(2))
-	assert.Equal(t, nil, ctx0.Value(2))
-	assert.Equal(t, 1, ctx01.Value(1))
-	assert.Equal(t, nil, ctx01.Value(2))
-
-	ctx021 := WithValue(ctx02, 3, 3)
-	assert.Equal(t, nil, ctx021.Value(1))
-	assert.Equal(t, 2, ctx021.Value(2))
-	assert.Equal(t, 3, ctx021.Value(3))
-}
-
-func TestValue_valueOverride(t *testing.T) {
-	t.Parallel()
-
-	ctx0 := WithValue(Background(), 0, 0)
-	ctx1 := WithValue(ctx0, 0, 1)
-
-	assert.Equal(t, 0, ctx0.Value(0))
-	assert.Equal(t, 1, ctx1.Value(0))
-}
-
-func TestConcurrency(t *testing.T) {
-	t.Parallel()
-
-	const n = 1000
-
-	ctx := WithValue(Background(), 0, 0)
-
-	var wg sync.WaitGroup
-	wg.Add(3)
-
-	go func(ctx Context) {
-		for i := 0; i < n; i++ {
-			ctx.Value(0)
-		}
-		wg.Done()
-	}(ctx)
-
-	go func(ctx Context) {
-		for i := 0; i < n; i++ {
-			ctx.Value(0)
-		}
-		wg.Done()
-	}(ctx)
-
-	go func(ctx Context) {
-		for i := 0; i < n; i++ {
-			WithValue(ctx, i, i)
-		}
-		wg.Done()
-	}(ctx)
-
-	for i := 0; i < n; i++ {
-		ctx = WithValue(ctx, i, i)
-	}
-
-	wg.Wait()
+	t.Run("WithValue", contexttest.TestWithValue(WithValue))
+	t.Run("WithValues", contexttest.TestWithValue(func(ctx Context, k, v interface{}) Context {
+		return WithValues(ctx, k, v)
+	}))
 }
 
 func TestWithValues(t *testing.T) {
--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/posener/fcontext
 
 go 1.12
 
-require github.com/stretchr/testify v1.3.0
+require (
+	github.com/posener/contexttest v0.1.0
+	github.com/stretchr/objx v0.2.0 // indirect
+	github.com/stretchr/testify v1.3.0
+)
