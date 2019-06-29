dec24de6b7edb1afb93a18b43f863b028ac38acc
Eyal Posener <posener@gmail.com>
2019-06-29 12:24:46 +0300

    contexttest nicer output

--- fcontext_test.go
+++ fcontext_test.go
@@ -8,16 +8,16 @@ import (
 )
 
 func TestWithValue(t *testing.T) {
-	t.Parallel()
-	t.Run("WithValue", contexttest.TestWithValue(WithValue))
-	t.Run("WithValues", contexttest.TestWithValue(func(ctx Context, k, v interface{}) Context {
-		return WithValues(ctx, k, v)
-	}))
+	contexttest.TestWithValue(WithValue)(t)
 }
 
 func TestWithValues(t *testing.T) {
+	contexttest.TestWithValue(func(ctx Context, k, v interface{}) Context {
+		return WithValues(ctx, k, v)
+	})(t)
+}
+func TestWithValues_multipleValues(t *testing.T) {
 	t.Parallel()
-
 	ctx := WithValues(Background(), 0, 0, 1, 1)
 	assert.Equal(t, 0, ctx.Value(0))
 	assert.Equal(t, 1, ctx.Value(1))
